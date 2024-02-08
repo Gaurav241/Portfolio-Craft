54dc8ddc02d7aa08f35009878daadd263e6accbf
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-02-08 19:56:51 +0530

    portfolio-craft: Hygraph: populates Professional Experiences

--- app/components/pages/home/works-experience/experience-item.tsx
+++ app/components/pages/home/works-experience/experience-item.tsx
@@ -1,14 +1,52 @@
 import { TechBadge } from '@/app/components/tech-badge'
+import { WorkExperience } from '@/app/types/work-experience'
+import { RichText } from '@graphcms/rich-text-react-renderer'
+import { differenceInMonths, differenceInYears, format } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
 import Image from 'next/image'
 
-export const ExperienceItem = () => {
+type ExperienceItemProps = {
+  experience: WorkExperience
+}
+
+export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
+  const {
+    companyName,
+    companyUrl,
+    companyLogo,
+    role,
+    endDate,
+    description,
+    technologies
+  } = experience
+
+  const startDate = new Date(experience.startDate)
+  const formattedStartDate = format(startDate, 'MMM yyyy', { locale: enUS })
+  const formattedEndDate = endDate
+    ? format(new Date(endDate), 'MMM yyyy', { locale: enUS })
+    : 'Present'
+
+  const end = endDate ? new Date(endDate) : new Date()
+
+  const months = differenceInMonths(end, startDate)
+  const years = differenceInYears(end, startDate)
+  const monthsRemaining = months % 12
+
+  const formattedDuration =
+    years > 0
+      ? `${years} year${years > 1 ? 's' : ''}${monthsRemaining > 0
+        ? ` and ${monthsRemaining} month${monthsRemaining > 1 ? 's' : ''}`
+        : ''
+      }`
+      : `${months} month${months > 1 ? 's' : ''}`
+
   return (
     <div className='grid grid-cols-[40px,1fr] gap-4 md:gap-10'>
       <div className='flex flex-col items-center gap-4'>
         <div className='rounded-full border border-gray-500 p-0.5'>
           <Image
-            src=''
-            alt='Company Logo'
+            src={companyLogo.url}
+            alt={`Company Logo ${companyName}`}
             width={40}
             height={40}
             className='rounded-full'
@@ -19,28 +57,30 @@ export const ExperienceItem = () => {
       <div>
         <div className='flex flex-col gap-2 text-sm sm:text-base'>
           <a
-            href='https://linkedin.com/krgauravgovinda'
+            href={companyUrl}
             target='_blank'
             className='text-gray-500 hover:text-emerald-500 transition-colors'
           >
-            @ Company Name
+            @ {companyName}
           </a>
-          <h4 className='text-gray-300'>Company Position</h4>
+          <h4 className='text-gray-300'>{role}</h4>
           <span className='text-gray-500'>
-            Oct 2022 - The Moment - (6 months)
+            {formattedStartDate} - {formattedEndDate} - ({formattedDuration})
           </span>
-          <p className='text-gray-400'>
-            The company itself is a very successful company.
-            Hate necessities, pleasures to the wise do not trouble,
-            something great with the desire to be free from these pains and troubles
-            times Like the pleasures he wanted the troubles of an easy mind?
-          </p>
+          <div className='text-gray-400'>
+            <RichText content={description.raw} />
+          </div>
         </div>
         <p className='text-gray-400 text-sm mb-3 mt-6 font-semibold'>
           Skills
         </p>
         <div className='flex gap-x-2 gap-y-3 flex-wrap lg:max-w-[350px] mb-8'>
-          <TechBadge name='PHP' />
+          {technologies.map((tech) => (
+            <TechBadge
+              key={`experience-${companyName}-tech-${tech.name}`}
+              name={tech.name}
+            />
+          ))}
         </div>
       </div>
     </div>
