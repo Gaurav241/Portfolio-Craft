c15d765e425aa0582945a8e87ce3f4004ae317ef
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-02-08 19:58:58 +0530

    portfolio-craft: Hygraph: populates the page of a specific project

--- app/components/pages/project/project-details/index.tsx
+++ app/components/pages/project/project-details/index.tsx
@@ -5,49 +5,52 @@ import { TbBrandGithub } from 'react-icons/tb'
 import { FiGlobe } from 'react-icons/fi'
 import { Link } from '@/app/components/link'
 import { HiArrowNarrowLeft } from 'react-icons/hi'
+import { Project } from '@/app/types/projects'
+import { RichText } from '@graphcms/rich-text-react-renderer'
 
-export const ProjectDetails = () => {
+type ProjectDetailsProps = {
+  project: Project
+}
+
+export const ProjectDetails = ({ project }: ProjectDetailsProps) => {
   return (
     <section className='w-full sm:min-h-[750px] flex flex-col items-center justify-end relative pb-10 sm:pb-24 py-24 px-6 overflow-hidden'>
       <div
         className='absolute inset-0 z-[-1]'
         style={{
-          background:
-            'url(/images/hero-bg.png) no-repeat center/cover, url(https://media.graphassets.com/7Kic5YHkQcmGrN57MSXw) no-repeat center/cover'
+          background: `url(/images/hero-bg.png) no-repeat center/cover, url(${project.pageThumbnail.url}) no-repeat center/cover`
         }}
       />
       <SectionsTitle
         subtitle='projects'
-        title='Project name'
+        title={project.title}
         className='text-center items-center sm:[&>h3]:text-4xl'
       />
-      <p className='text-gray-400 text-center max-w-[640px] my-4 sm:my-6 text-sm sm:text-base'>
-        The company itself is a very successful company. His pains
-        in they never know resilience, which is what it is! From our wise offices
-        Therefore we accuse the blessed one of the easy consequences of the matter, as he wills it.
-      </p>
+      <div className='text-gray-400 text-center max-w-[640px] my-4 sm:my-6 text-sm sm:text-base'>
+        <RichText content={project.description.raw} />
+      </div>
       <div className='w-full max-w-[330px] flex flex-wrap gap-2 items-center justify-center'>
-        <TechBadge name='Node.js' />
-        <TechBadge name='Node.js' />
-        <TechBadge name='Node.js' />
-        <TechBadge name='Node.js' />
-        <TechBadge name='Node.js' />
-        <TechBadge name='Node.js' />
-        <TechBadge name='Node.js' />
+        {project.technologies.map((technology) => (
+          <TechBadge key={technology.name} name={technology.name} />
+        ))}
       </div>
       <div className='my-6 sm:my-12 flex items-center gap-2 sm:gap-4 flex-col sm:flex-row'>
-        <a href='https://github.com/Gaurav241' target='_blank'>
-          <Button className='min-w-[180px]'>
-            <TbBrandGithub size={20} />
-            Repository
-          </Button>
-        </a>
-        <a href='https://github.com/Gaurav241' target='_blank'>
-          <Button className='min-w-[180px]'>
-            <FiGlobe size={20} />
-            Online Project
-          </Button>
-        </a>
+        {project.githubUrl && (
+          <a href={project.githubUrl} target='_blank'>
+            <Button className='min-w-[180px]'>
+              <TbBrandGithub size={20} />
+              Repository
+            </Button>
+          </a>
+        )}
+        {project.liveProjectUrl && (
+          <a href={project.liveProjectUrl} target='_blank'>
+            <Button className='min-w-[180px]'>
+              <FiGlobe size={20} />
+              Online Project
+            </Button>
+          </a>
+        )}
       </div>
       <Link href='/projects'>
         <HiArrowNarrowLeft size={20} />
