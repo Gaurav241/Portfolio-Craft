46853c71599380e24b69a389ede0310e41db6ffd
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-02-08 18:59:12 +0530

    portfolio-craft: Add Component: KnownTechs

---
+++ app/components/pages/home/known-techs/known-tech/index.tsx
@@ -0,0 +1,24 @@
+import { getRelativeTimeString } from '@/app/utils/get-relative-time';
+import { ReactNode } from 'react';
+
+type KnownTechProps = {
+  tech: {
+    icon: ReactNode
+    name: string
+    startDate: string
+  }
+}
+
+export const KnownTech = ({ tech }: KnownTechProps) => {
+  const relativeTime = getRelativeTimeString(new Date(tech.startDate), 'pt-BR').replace('há ', '')
+
+  return (
+    <div className='p-6 rounded-lg bg-gray-600/20 text-gray-500 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all'>
+      <div className='flex items-center justify-between'>
+        <p className='font-medium'>{tech.name}</p>
+        {tech.icon}
+      </div>
+      <span>{relativeTime} of experience</span>
+    </div>
+  );
+}
