7d5cac75f19ad263c74fe9308f6903ea0f3ca390
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-02-08 19:20:51 +0530

    portfolio-craft: Add Component: ContactForm

---
+++ app/components/contact-form/index.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Button } from '../button'
+import { SectionsTitle } from '../sections-title'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+export const ContactForm = () => {
+  const contactFormSchema = z.object({
+    name: z.string().min(3).max(100),
+    email: z.string().email(),
+    message: z.string().min(1).max(500)
+  })
+
+  type ContactFormData = z.infer<typeof contactFormSchema>
+
+  const { register, handleSubmit } = useForm<ContactFormData>({
+    resolver: zodResolver(contactFormSchema)
+  })
+
+  const onSubmit = (data: ContactFormData) => {
+    console.log(data)
+  }
+
+  return (
+    <section className='py-16 px-6 md:py-32 flex items-center justify-center bg-gray-950'>
+      <div className='w-full max-w-[420px] mx-auto'>
+        <SectionsTitle
+          subtitle='contact'
+          title='Lets work together? Contact'
+        className='items-center text-center'
+         />
+        <form
+          className='mt-12 w-full flex flex-col gap-4'
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            autoComplete='off'
+            type='text'
+            placeholder='Name'
+            className='w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600 transition-all '
+            {...register('name')}
+          />
+          <input
+            autoComplete='off'
+            type='email'
+            placeholder='Email'
+            className='w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald-600 transition-all '
+            {...register('email')}
+          />
+          <textarea
+            autoComplete='off'
+            placeholder='Message...'
+            className='resize-none w-full h-[138px] bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 ring-emerald -600 transition-all'
+            maxLength={500}
+            {...register('message')}
+          />
+          <Button className='mt-6 w-max mx-auto shadow-button'>
+            Send Message
+            <HiArrowNarrowRight size={18} />
+          </Button>
+        </form>
+      </div>
+    </section>
+  )
+}
---
+++ app/components/footer/index.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className='h-14 w-full flex items-center justify-center bg-gray-950'>
+      <span className='flex items-center gap-1.5 text-xs sm:text-sm font-mono text-gray-400'>
+        Made by <strong className='font-medium'>Gaurav Govinda</strong>
+      </span>
+    </footer>
+  )
+}
