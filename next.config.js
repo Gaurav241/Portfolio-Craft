d068aa278e26e35c8afa2ad4aa2260b7afefbec6
Gaurav241 <gaurav.govinda2420@gmail.com>
2024-03-14 21:41:01 +0530

    Portfolio-Craft: Fix Invalid next.config.js options.
    - Unrecognized key(s) in object: 'appDir' at "experimental"
    - The "images.domains" configuration is deprecated. Please use "images.remotePatterns" configuration instead.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['media.graphassets.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.graphassets.com',
+        pathname: '**',
+      },
+    ],
   },
 }
 
