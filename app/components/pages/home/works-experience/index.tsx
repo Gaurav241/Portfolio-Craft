379155700b545ca6bed59e034c3012e938d7b2a2
Gaurav241 <gaurav.govinda2420@gmail.com>
2025-05-22 12:52:17 +0530

    portfolio-craft: Short Experience by date.

--- app/components/pages/home/works-experience/index.tsx
+++ app/components/pages/home/works-experience/index.tsx
@@ -7,10 +7,16 @@ type WorksExperienceProps = {
 }
 
 export const WorksExperience = ({ experiences }: WorksExperienceProps) => {
+  const sortedExperiences = [...experiences].sort((a, b) => {
+    const dateA = new Date(a.startDate).getTime()
+    const dateB = new Date(b.startDate).getTime()
+    return dateB - dateA
+  })
+
   return (
-    <section 
-       id='experiences'
-       className='container py-16 flex gap-10 md:gap-4 lg:gap-16 flex-col md:flex-row'
+    <section
+      id='experiences'
+      className='container py-16 flex gap-10 md:gap-4 lg:gap-16 flex-col md:flex-row'
     >
       <div className='max-w-[420px]'>
         <SectionsTitle
@@ -18,11 +24,11 @@ export const WorksExperience = ({ experiences }: WorksExperienceProps) => {
           title='Professional Experiences'
         />
         <p className='text-gray-400 mt-6'>
-        Always open to teaming up and growing together, because let's face it, I can't reach the top shelf on my own.
+          Always open to teaming up and growing together, because let's face it, I can't reach the top shelf on my own.
         </p>
       </div>
       <div className='flex flex-col gap-4'>
-        {experiences?.map((experience) => (
+        {sortedExperiences?.map((experience) => (
           <ExperienceItem key={experience.companyName} experience={experience} />
         ))}
       </div>
